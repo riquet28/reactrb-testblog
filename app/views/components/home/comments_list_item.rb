7937a39ca4f0893e2383e5940e146837f7ed3689
riquet28 <eric.merlin3@gmail.com>
2016-09-11 02:54:39 +0200

    Add function edit for Comment

--- app/views/components/home/comments_list_item.rb
+++ app/views/components/home/comments_list_item.rb
@@ -1,15 +1,24 @@
 module Components
   module Home
-
     class CommentsListItem < React::Component::Base
+
       param :comment
+
+      define_state editing_comment: false
 
       def render
         li do
           ReactBootstrap::Button(bsClass: "btn btn-default pull-right") do
-            "Détruire ce Commentaire"
+            "Supprimer"
           end.on(:click) { params.comment.destroy }
-          p{params.comment.body}
+          ReactBootstrap::Button(bsClass: "btn btn-success pull-right") do
+            "Editer"
+          end.on(:click) { state.editing_comment! true }
+          if state.editing_comment
+            CommentEditItem(comment: params.comment).on(:save) { state.editing_comment! false}
+          else
+            p{params.comment.body}.on(:doubleClick) { state.editing_comment! true }
+          end
         end
       end
 
