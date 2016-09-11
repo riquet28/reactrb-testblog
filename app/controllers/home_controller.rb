1bfd2c3197b4829581d3dc377f47babde250e11b
riquet28 <eric.merlin3@gmail.com>
2016-09-11 22:12:02 +0200

    Amélioration ok

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,4 @@
 class HomeController < ApplicationController
   def show
   end
-  def welcome
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   mount ReactiveRecord::Engine => '/rr'
+
+  devise_for :users
 
   root 'home#show'
 
