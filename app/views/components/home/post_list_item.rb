8f34a066d0bdb3a77f1157d81fbffea5a12bdb98
riquet28 <eric.merlin3@gmail.com>
2016-09-18 22:50:27 +0200

    Edit Post ok

--- app/views/components/home/post_list_item.rb
+++ app/views/components/home/post_list_item.rb
@@ -1,6 +1,7 @@
 module Components
   module Home
     class PostListItem < React::Component::Base
+
       param :post, type: Post
       param :current_user, type:User
 
@@ -8,39 +9,43 @@ module Components
 
       def render
         li do
-          if state.editing_post
-            PostEditItem(post: params.post).on(:save) { state.editing_post! false}
-          else
-            div.row do
-              div(class: "col-md-12 margin-btm-20") do
-                div(class: "quote green") do
-                  blockquote do
-                    p(class: "post-paragraphe"){ "Posté le ... A FAIRE au bon format ... #{params.post.created_at} par #{params.post.user.email}" }
-                    p { "Titre du Post : \"#{params.post.body}...\"" }.on(:doubleClick) { state.editing_post! true }
-                    if params.post.user.id == current_user.id
-                      div.row do
-                        div(class: "col-md-3 col-md-offset-9") do
-                          ReactBootstrap::Button(bsClass: "btn btn-danger post-button") do
-                            "Supprimer"
-                          end.on(:click) { destroy_post }
-                          ReactBootstrap::Button(bsClass: "btn btn-success post-button") do
-                            "Modifier"
-                          end.on(:click) { state.editing_post! true }
+          div.row do
+            div(class: "col-md-12 margin-btm-20") do
+              div(class: "quote green") do
+                blockquote do
+                  div.row do
+                    div.col_md_9 do
+                      p(class: "post-paragraphe"){ "Posté le ... A FAIRE au bon format ... #{params.post.created_at} par #{params.post.user.email}" }
+                      p { "Titre du Post : \"#{params.post.body}...\"" }.on(:doubleClick) { state.editing_post! true }
+                      if params.post.user.id == current_user.id
+                        if state.editing_post
+                          PostEditItem(post: params.post).on(:save) { state.editing_post! false}
                         end
-
+                      end
+                    end
+                    div.col_md_3.text_center do
+                      if params.post.user.id == current_user.id
+                        ReactBootstrap::Button(bsClass: "btn btn-danger post-button") do
+                          "Supprimer"
+                        end.on(:click) { destroy_post }
+                        ReactBootstrap::Button(bsClass: "btn btn-success post-button") do
+                          "Modifier"
+                        end.on(:click) { state.editing_post! true }
+                      else
+                        span.badge {"Non modifiable"}
                       end
                     end
                   end
                 end
-                div(class: "quote-footer text-right") do
-                  div(class: "quote-author-img") do
-                    img
-                  end
-                  if post.comments.blank?
-                    h4 {"Il n'y a pas encore de commentaire pour ce post !"}
-                  else
-                    h4 {"Les commentaires pour ce post : #{post.comments.count}"}
-                  end
+              end
+              div(class: "quote-footer text-right") do
+                div(class: "quote-author-img") do
+                  img
+                end
+                if post.comments.blank?
+                  h4 {"Il n'y a pas encore de commentaire pour ce post !"}
+                else
+                  h4 {"Les commentaires pour ce post : #{post.comments.count}"}
                 end
               end
             end
